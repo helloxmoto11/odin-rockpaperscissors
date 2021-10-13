56d1e0ece47d28a0c9ffc682d1f5f9596d6214d7
Manuel Caravantes <rhcplive11@gmail.com>
2021-10-13 11:03:39 -0500

    finally have a working game

--- index.js
+++ index.js
@@ -8,30 +8,28 @@ const buttons = Array.from(document.getElementsByClassName("btn-play"));
 let results = Array.from(document.getElementsByClassName("result"));
 let resultText = results[0];
 
+// you chose line
 let youChose = document.querySelector('#you-chose');
+//shows what round
 let round = document.querySelector('.round');
-
-
-
+//shows score
 const scoreElement = document.querySelector('.score');
 
 const retryBtnContainer = document.querySelector('.btn-rty-container');
 
-const retryButton = document.querySelector('#btn-retry');
-
-
 function setClickListener(e) {
-    let playerPlay = this.id
-    const computersPlay = computerPlay();
-    youChose.innerHTML = "You Chose " + playerPlay;
+    let playerChoice = this.id
+    const computersChoice = computerPlay();
+    youChose.innerHTML = "You Chose " + playerChoice;
 
-    result = playRound(playerPlay, computersPlay);
+    result = playRound(playerChoice, computersChoice);
     resultText.innerHTML = result;
     
     scoreElement.innerHTML = "Score: " + score;
-    console.log(score);
+
     buttons.forEach(button => button.disabled = true);
     displayRetryButton();
+    if (roundCount == 3) displayGameOver();
 }
 
 buttons.forEach(button => button.addEventListener('click', setClickListener))
@@ -41,15 +39,38 @@ function displayRetryButton() {
     const button = document.createElement('button');
     button.className = 'btn';
     button.id = 'btn-retry';
-    button.innerText = 'Play Again?'
+    if (roundCount != 3) {
+        button.innerText = 'Retry?'
+    } else button.innerText = "Play Again?"
     retryBtnContainer.appendChild(button);
 
     button.addEventListener('click', function() {
-        roundCount++;
+        if (roundCount == 3) {
+            roundCount = 0;
+            score = 0;
+            scoreElement.innerText = "Score: " + score;
+        }
+
+        roundCount++;        
         round.innerHTML = "Round: " + roundCount;
         buttons.forEach(btn => btn.disabled = false);
         retryBtnContainer.removeChild(button);
+        removeGameOver();
+
     });
+}
+
+let gameOverElement;
+function displayGameOver() {
+    gameOverElement = document.createElement('h1');
+    gameOverElement.id = 'game-over';
+    gameOverElement.innerText = "Game Over!"
+    document.querySelector('#g-div').appendChild(gameOverElement);
+}
+
+function removeGameOver() {
+    console.log("Remove Child Function");
+    document.querySelector('#g-div').removeChild(gameOverElement);
 }
 
 
